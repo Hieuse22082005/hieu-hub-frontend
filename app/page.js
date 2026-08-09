231fad177e48dfafaded5670d6c472a605d8d751
Hieuse22082005 <duongxuanhieu22082005@gmail.com>
2026-08-09 15:19:01 +0700

    Update page.js

--- app/page.js
+++ app/page.js
@@ -781,7 +781,7 @@ export default function HieuHubMaster() {
         </AnimatePresence>
       </div>
       
-      <style dangerouslySetContent={{__html: `
+      <style dangerouslySetInnerHTML={{__html: `
         .custom-scrollbar::-webkit-scrollbar { width: 4px; }
         .custom-scrollbar::-webkit-scrollbar-track { background: transparent; }
         .custom-scrollbar::-webkit-scrollbar-thumb { background: rgba(255,255,255,0.1); border-radius: 10px; }
